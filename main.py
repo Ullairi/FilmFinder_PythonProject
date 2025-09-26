eca37c2a97d6e24ab6d914cbf9472427d3c0840d
Ullairi <maks.tih2005@gmail.com>
2025-09-26 14:07:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import dotenv
+from pathlib import Path
+import pymysql
+
+
+dotenv.load_dotenv(Path('.env'))
+dbconfig = {'host': os.environ.get('host'),
+            'user': os.environ.get('user'),
+            'password': os.environ.get('password'),
+            'database': os.environ.get('db_sql')}
+
+connection = pymysql.connect(**dbconfig)
+cursor = connection.cursor()
+
+cursor.execute(
+'''
+    show tables
+''')
+
+result = cursor.fetchall()
+for num, table in enumerate(result, start=1):
+    print(f"{num}: {table[0]}")
+table = input("Choose table: ")
+
+cursor.execute(f'SELECT * FROM {table}')
+result = cursor.fetchall()
+print(*result, sep='\n')
+
+cursor.close()
+connection.close()

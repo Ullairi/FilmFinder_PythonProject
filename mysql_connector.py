de9557b6138f36ffda67acf6690d83ec31268831
Ullairi <maks.tih2005@gmail.com>
2025-09-27 15:17:23 +0100

    Added functions to get genres list and year range

--- mysql_connector.py
+++ mysql_connector.py
@@ -29,3 +29,30 @@ def search_by_keyword(keyword, limit=10,offset=0):
         cursor = conn.cursor()
         cursor.execute(search_query, (f"%{keyword}%",limit,offset))
         return cursor.fetchall()
+
+
+# Get list of all available genres
+def genre_list():
+    genre_query = """
+    SELECT category_id, name
+    FROM category
+    ORDER BY name;
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+        cursor.execute(genre_query)
+        return cursor.fetchall()
+
+
+# Get min. and max. release year range from films
+def year_range():
+    year_query = """
+    SELECT MIN(release_year) AS min_year, MAX(release_year) AS max_year
+    FROM film;
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+        cursor.execute(year_query)
+        return cursor.fetchone()

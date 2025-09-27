e08cf6006325cfcf6928139767ba25e37feeca78
Ullairi <maks.tih2005@gmail.com>
2025-09-27 22:43:44 +0100

    Added 'main_menu()' function to display options to user. Prepared structure for future features (genre search and exit)

--- main.py
+++ main.py
@@ -2,30 +2,31 @@ import os
 import dotenv
 from pathlib import Path
 import pymysql
+from formatter import search_films_keyword
 
 
-dotenv.load_dotenv(Path('.env'))
-dbconfig = {'host': os.environ.get('host'),
-            'user': os.environ.get('user'),
-            'password': os.environ.get('password'),
-            'database': os.environ.get('db_sql')}
+# Main Menu function for Film Finder
+# Display options for the user to choose from and return their choice
 
-connection = pymysql.connect(**dbconfig)
-cursor = connection.cursor()
+def main_menu():
+    print("\n-------------- FIlM FINDER --------------")
+    print("1. Search films by keyword")
+    print("2. Search films by genre and year range")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    return choice.strip()
 
-cursor.execute(
-'''
-    show tables
-''')
 
-result = cursor.fetchall()
-for num, table in enumerate(result, start=1):
-    print(f"{num}: {table[0]}")
-table = input("Choose table: ")
+if __name__ == "__main__":
+    while True:
+        choice = main_menu()
 
-cursor.execute(f'SELECT * FROM {table}')
-result = cursor.fetchall()
-print(*result, sep='\n')
-
-cursor.close()
-connection.close()
+        if choice == "1":
+            search_films_keyword()
+        # elif choice == "2":
+        #     print("Search films by genre and year range")
+        # elif choice == "3":
+        #     print("Thank you for using this program. Goodbye!")
+        #     break
+        else:
+            print("Invalid choice. Please try again.")

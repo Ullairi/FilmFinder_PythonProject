746c76ad9f5bacf461c8c02f8ef8fb420d6dd9c8
Ullairi <maks.tih2005@gmail.com>
2025-09-27 15:45:51 +0100

    Added function to find films by genre and release year range

--- mysql_connector.py
+++ mysql_connector.py
@@ -15,14 +15,14 @@ dbconfig = {'host': os.environ.get('host'),
 def connect():
     return pymysql.connect(**dbconfig)
 
-# Find film by keyword function
+# Find films by keyword function
 def search_by_keyword(keyword, limit=10,offset=0):
     search_query = """
-                SELECT film_id, title
-                FROM film
-                WHERE title LIKE %s
-                ORDER BY title
-                LIMIT %s OFFSET %s;
+            SELECT film_id, title
+            FROM film
+            WHERE title LIKE %s
+            ORDER BY title
+            LIMIT %s OFFSET %s;
     """
 
     with connect() as conn:
@@ -34,9 +34,9 @@ def search_by_keyword(keyword, limit=10,offset=0):
 # Get list of all available genres
 def genre_list():
     genre_query = """
-    SELECT category_id, name
-    FROM category
-    ORDER BY name;
+            SELECT category_id, name
+            FROM category
+            ORDER BY name;
     """
 
     with connect() as conn:
@@ -48,11 +48,29 @@ def genre_list():
 # Get min. and max. release year range from films
 def year_range():
     year_query = """
-    SELECT MIN(release_year) AS min_year, MAX(release_year) AS max_year
-    FROM film;
+            SELECT MIN(release_year) AS min_year, MAX(release_year) AS max_year
+            FROM film;
     """
 
     with connect() as conn:
         cursor = conn.cursor()
         cursor.execute(year_query)
         return cursor.fetchone()
+
+
+# Find films by genre and release year range
+def search_by_genre_year(category_id, min_year, max_year, limit=10, offset=0):
+    genre_year_query = """
+            SELECT f.film_id, f.title, f.release_year, f.description
+            FROM film f
+            JOIN film_category fc ON fc.film_id = f.film_id
+            WHERE fc.category_id = %s
+            AND f.release_year BETWEEN %s AND %s
+            ORDER BY f.release_year, f.title
+            LIMIT %s OFFSET %s;
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+        cursor.execute(genre_year_query, (category_id,min_year, max_year,limit,offset))
+        return cursor.fetchall()

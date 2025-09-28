3936fc9c6cc7abf0f77be3c12484f5d11af50cb1
Ullairi <maks.tih2005@gmail.com>
2025-09-28 13:18:01 +0100

    Added search by genre and year option

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import dotenv
 from pathlib import Path
 import pymysql
-from formatter import search_films_keyword
+from formatter import search_films_keyword, search_films_by_genre_and_year
 
 
 # Main Menu function for Film Finder
 # Display options for the user to choose from and return their choice
 
 def main_menu():
-    print("\n-------------- FIlM FINDER --------------")
+    print("\n< ------------ FIlM FINDER ------------ 2>")
     print("1. Search films by keyword")
     print("2. Search films by genre and year range")
     print("3. Exit")
@@ -23,10 +23,10 @@ if __name__ == "__main__":
 
         if choice == "1":
             search_films_keyword()
-        # elif choice == "2":
-        #     print("Search films by genre and year range")
-        # elif choice == "3":
-        #     print("Thank you for using this program. Goodbye!")
-        #     break
+        elif choice == "2":
+            search_films_by_genre_and_year()
+        elif choice == "3":
+            print("Thank you for using this program. Goodbye!")
+            break
         else:
             print("Invalid choice. Please try again.")

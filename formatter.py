6b5fe6a45ba7c739aaeabdb8845fb3526363f29f
Ullairi <maks.tih2005@gmail.com>
2025-09-28 20:12:25 +0100

    Added checks to validate user input and immediately notify on invalid values

--- formatter.py
+++ formatter.py
@@ -22,7 +22,7 @@ class Formatter:
                 print("\n< ID | Title >") # Header title for first row
                 header = True
 
-            # Show list of films and print formatted info
+#           //Show list of films and print formatted info
             for film in films:
                 film_id, title, description = film
                 print(f"# {film_id} | {title}\nDescription:\n{description}\n" + "-" * 70)
@@ -30,30 +30,49 @@ class Formatter:
             if len(films) < limit:
                 break
 
-            # Ask user if he wants to see 10 next results
+#           //Ask user if he wants to see 10 next results
             count = input("Show next 10 films? (y/n): ").lower()
             if count != "y":
                 break
             offset += limit
 
 
-    # Function that shows available genres
+#        //Function that shows available genres
     def  search_films_by_genre_and_year(self):
         print("\n<-- Available Genres -->")
         genres = self.db.genre_list()
         for g in genres:
             print(f"{g[0]}: {g[1]}")
 
-    #   Shows available year range
+#       //Shows available year range
         year = self.db.year_range()
         print(f"\n<-- Available year range: {year[0]} - {year[1]} -->")
 
-        try: # Ask user for genre id and year range
+
+        try: # Ask user for genre id
             category_id = int(input("Enter genre category ID: "))
-            min_year = int(input("Enter start year: "))
-            max_year = int(input("Enter end year: "))
         except ValueError: # If user will type non-numeric input
             print("Invalid input. Please enter only numbers")
+            return
+
+#       //Category_id check
+        categ_id = [genre[0] for genre in genres]
+        if category_id not in categ_id:
+            print(f"Invalid category ID. Please choose category from {categ_id}.")
+            return
+
+
+        try: # Ask user for year range
+            min_year = int(input("Enter start year: "))
+            max_year = int(input("Enter end year: "))
+        except ValueError:
+            print("Invalid input. Please enter only numbers")
+            return
+
+#       //Year range check
+        minimal_year, maximal_year = year[0], year[1]
+        if min_year < minimal_year or max_year > maximal_year or min_year > max_year:
+            print(f"Invalid input. Please enter years within the {minimal_year} - {maximal_year} range")
             return
 
         offset = 0
@@ -77,7 +96,7 @@ class Formatter:
             if len(films) < limit:
                 break
 
-            # Ask user if he wants to see 10 next results
+#           //Ask user if he wants to see 10 next results
             count = input("Show next 10 films? (y/n): ").lower()
             if count != "y":
                 break

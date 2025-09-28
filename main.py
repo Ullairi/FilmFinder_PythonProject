4706ac411ca7a033ef3369280fca3fbd01b27067
Ullairi <maks.tih2005@gmail.com>
2025-09-28 19:23:23 +0100

    main.py mow use Formatter class for cleaner code view. Created Formatter object for cleaner method calls.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-import dotenv
-from pathlib import Path
-import pymysql
-from formatter import search_films_keyword, search_films_by_genre_and_year
+from formatter import Formatter
 
+fr = Formatter() # Created class object
 
 # Main Menu function for Film Finder
 # Display options for the user to choose from and return their choice
@@ -16,15 +13,15 @@ def main_menu():
     choice = input("Enter your choice: ")
     return choice.strip()
 
-
+# Entry point of the program
 if __name__ == "__main__":
     while True:
         choice = main_menu()
 
         if choice == "1":
-            search_films_keyword()
+            fr.search_films_keyword()
         elif choice == "2":
-            search_films_by_genre_and_year()
+            fr.search_films_by_genre_and_year()
         elif choice == "3":
             print("Thank you for using this program. Goodbye!")
             break

e1704fdb2df2f0df10d797bd3fde7ed0c708eb9a
Ullairi <maks.tih2005@gmail.com>
2025-09-29 19:00:16 +0100

    main.py now connected with LogStats Expended main menu features to handle user input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from log_stats import LogStats
 from formatter import Formatter
 
-fr = Formatter() # Created class object
+fr = Formatter() # Created Formatter instance to handle search and display
+stats = LogStats() # Created LogStats instance to handle statistics search
 
 # Main Menu function for Film Finder
 # Display options for the user to choose from and return their choice
@@ -9,7 +11,9 @@ def main_menu():
     print("\n< ------------ FIlM FINDER ------------ 2>")
     print("1. Search films by keyword")
     print("2. Search films by genre and year range")
-    print("3. Exit")
+    print("3. Show top 5 search results")
+    print("4. Show last 5 search results")
+    print("5. Exit")
     choice = input("Enter your choice: ")
     return choice.strip()
 
@@ -19,11 +23,19 @@ if __name__ == "__main__":
         choice = main_menu()
 
         if choice == "1":
-            fr.search_films_keyword()
+            fr.search_films_keyword() # Search film using keyword
         elif choice == "2":
-            fr.search_films_by_genre_and_year()
+            fr.search_films_by_genre_and_year() # Search film by genre and year range
         elif choice == "3":
+            top = stats.top_queries(5) # Shows top 5 search queries
+            print("\nTop 5 search results:")
+            stats.queries_output(top)
+        elif choice == "4":
+           last = stats.last_queries(5) # Shows last 5 search queries
+           print("\nLast 5 search results:")
+           stats.queries_output(last)
+        elif choice == "5":
             print("Thank you for using this program. Goodbye!")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice. Please try again.") # In case of invalid input

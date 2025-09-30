16e7989da4085d3712f77f58b7cd9c9721fcce56
Ullairi <maks.tih2005@gmail.com>
2025-09-30 11:05:33 +0100

    Added statistics submenu for last and top searches Improved user navigation between main and statistics menu

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ stats = LogStats() # Created LogStats instance to handle statistics search
 # Display options for the user to choose from and return their choice
 
 def main_menu():
-    print("\n< ------------ FIlM FINDER ------------ 2>")
+    print("\n< ------------ FIlM FINDER ------------ >")
     print("1. Search films by keyword")
     print("2. Search films by genre and year range")
-    print("3. Show top 5 search results")
-    print("4. Show last 5 search results")
-    print("5. Exit")
+    print("3. Statistics")
+    print("4. Exit")
     choice = input("Enter your choice: ")
+    return choice.strip()
+
+def log_menu():
+    print("\n< ------------ Statistics ------------ >")
+    print("1. Top 5 searches")
+    print("2. Last 5 searches")
+    print("3. Return to main menu")
+    choice = input("Enter your choice: ").strip()
     return choice.strip()
 
 # Entry point of the program
@@ -27,14 +34,23 @@ if __name__ == "__main__":
         elif choice == "2":
             fr.search_films_by_genre_and_year() # Search film by genre and year range
         elif choice == "3":
-            top = stats.top_queries(5) # Shows top 5 search queries
-            print("\nTop 5 search results:")
-            stats.queries_output(top)
+            while True:
+                log_choice = log_menu()
+                if log_choice == "1":
+                    top = stats.top_queries(5) # Shows top 5 search queries
+                    print("\nTop 5 search results:")
+                    stats.queries_output(top)
+                    input("\nPress enter to continue...")
+                elif log_choice == "2":
+                    last = stats.last_queries(5)
+                    print("\nLast 5 search results:")
+                    stats.queries_output(last)
+                    input("\nPress enter to return to statistics menu...")
+                elif log_choice == "3":
+                    break
+                else:
+                    print("\nInvalid choice. Try again")
         elif choice == "4":
-           last = stats.last_queries(5) # Shows last 5 search queries
-           print("\nLast 5 search results:")
-           stats.queries_output(last)
-        elif choice == "5":
             print("Thank you for using this program. Goodbye!")
             break
         else:
